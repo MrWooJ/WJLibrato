16549aa8fc3fd5a78cd40b7c07982272b8a86a33
MrWooJ <hurricanc@gmail.com>
2015-09-29 13:20:56 +0330

    Create Project File and Targer

---
+++ WJLibrato/WJLibrato/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WJLibrato
+//
+//  Created by Alireza Arabi on 9/29/15.
+//  Copyright © 2015 Alireza Arabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WJLibrato/WJLibrato/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WJLibrato
+//
+//  Created by Alireza Arabi on 9/29/15.
+//  Copyright © 2015 Alireza Arabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
